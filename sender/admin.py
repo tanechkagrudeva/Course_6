09131a3eca9e7f0f72f6dec7bb4cf1259cdbe509
roman <gadjka@mail.ru>
2023-07-10 10:54:44 +0500

    add models

--- sender/admin.py
+++ sender/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from sender.models import Client, Mailing, MailingMessage, MailingLog
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'patronymic', 'email', 'comment',)
+    list_filter = ('last_name', 'email',)
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('send_time', 'send_period', 'send_status',)
+    list_filter = ('send_time', 'send_period', 'send_status',)
+
+
+@admin.register(MailingMessage)
+class MailingMessageAdmin(admin.ModelAdmin):
+    list_display = ('mailing', 'subject', 'body',)
+    list_filter = ('mailing', 'subject', 'body',)
+
+
+@admin.register(MailingLog)
+class MailingLogAdmin(admin.ModelAdmin):
+    list_display = ('mailing', 'timestamp', 'status', 'response',)
+    list_filter = ('mailing', 'timestamp', 'status', 'response',)
--- sender/models.py
+++ sender/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=50, verbose_name='имя')
+    last_name = models.CharField(max_length=50, verbose_name='фамилия')
+    patronymic = models.CharField(max_length=50, verbose_name='отчество')
+
+    email = models.EmailField(max_length=150, verbose_name='почта')
+    comment = models.TextField(verbose_name='комментарий')
+
+    def __str__(self):
+        return f'{self.first_name} ({self.email})'
+
+    class Meta:
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
+
+
+class Mailing(models.Model):
+    SEND_PERIOD_CHOICES = (
+        ('daily', 'Раз в день'),
+        ('weekly', 'Раз в неделю'),
+        ('monthly', 'Раз в месяц'),
+    )
+    SEND_STATUS_CHOICES = (
+        ('created', 'Создана'),
+        ('started', 'Запущена'),
+        ('completed', 'Завершена'),
+    )
+    send_time = models.TimeField(verbose_name='время рассылки')
+    send_period = models.CharField(max_length=7, choices=SEND_PERIOD_CHOICES, verbose_name='периодичность')
+    send_status = models.CharField(max_length=9, choices=SEND_STATUS_CHOICES, verbose_name='статус')
+
+    def __str__(self):
+        return f'{self.send_time} - {self.send_period} - {self.send_status}'
+
+    class Meta:
+        verbose_name = 'настройка'
+        verbose_name_plural = 'настройки'
+
+
+class MailingMessage(models.Model):
+    mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE, verbose_name='настройка')
+    subject = models.CharField(max_length=255, verbose_name='тема')
+    body = models.TextField(verbose_name='текст письма')
+
+    def __str__(self):
+        return f'{self.subject}'
+
+    class Meta:
+        verbose_name = 'рассылка'
+        verbose_name_plural = 'рассылки'
+
+
+class MailingLog(models.Model):
+    mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE, verbose_name='настройка')
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name='дата пследней рассылки')
+    status = models.CharField(max_length=255, verbose_name='статус')
+    response = models.TextField(blank=True, null=True, verbose_name='ответ сервера')
+
+    def __str__(self):
+        return f'{self.timestamp} - {self.status}'
+
+    class Meta:
+        verbose_name = 'попытка'
+        verbose_name_plural = 'попытки'
